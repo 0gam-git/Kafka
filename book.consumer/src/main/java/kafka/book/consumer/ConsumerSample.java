8eb4a261c46c434a7de58de479926c4bc06e7a01
byeon0gam <dudwns519@daum.net>
2018-05-10 17:01:30 +0900

    카프카 컨슈머

---
+++ book.consumer/src/main/java/kafka/book/consumer/ConsumerSample.java
@@ -0,0 +1,40 @@
+package kafka.book.consumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class ConsumerSample {
+
+	public static void main(String[] args) {
+
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "203.251.177.28:9092");
+		props.put("group.id", "peter-consumer");
+		props.put("enable.auto.commit", "true");
+		props.put("auto.offset.reset", "latest");
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+		consumer.subscribe(Arrays.asList("peter-topic"));
+
+		try {
+			while (true) {
+				ConsumerRecords<String, String> records = consumer.poll(100);
+
+				for (ConsumerRecord<String, String> record : records) {
+					System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(),
+							record.value());
+				}
+			}
+
+		} finally {
+			consumer.close();
+		}
+	}
+
+}
